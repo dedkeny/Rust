bdd00c26f3c9f4758899a251ac181d398740de4b
Dedkeny <kennethalmas232@gmail.com>
2024-10-09 19:26:11 -0400

    reading sec 1

--- hello_there/src/main.rs
+++ hello_there/src/main.rs
@@ -1,3 +1,12 @@
+// BS programm to run through the Rust Documentation
+
+// 1.   - Hello world
+
 fn main() {
+    // 1.1  - Comments, IYKYK
     println!("Hello, world!");
+
+    // 1.2  - Formatted Print
+    let x = "World";
+    println!("Hello, {}!", x);
 }
