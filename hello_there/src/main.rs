fae6d2a6b683f0c728db82bef50c93bb82d0e11b
Keny <dedkeny@pop-os.lan>
2024-10-15 17:35:33 -0400

    Being a good boy...

--- hello_there/src/main.rs
+++ hello_there/src/main.rs
@@ -3,10 +3,18 @@
 // 1.   - Hello world
 
 fn main() {
-    // 1.1  - Comments, IYKYK
+    /*
+      1.1  - Comments, IYKYK
+    */
     println!("Hello, world!");
-
+    println!("I'm a Rustagen");
     // 1.2  - Formatted Print
-    let x = "World";
-    println!("Hello, {}!", x);
+    let x = 5 + /* 90 + */ 5;
+    println!("Comment Question:\n\tlet x = 5 + /* 90 + */ 5;\n");
+    println!("Is 'x' 10 or 100?\n\tx = {}\n", x);
+
+    println!("{} was an int before being stringified into thi statement", 31);
+    println!("{0} is before {1} because {1}st place doesn't matter", 0, 1);
+
+
 }
