fcef1742adafa8d140e896b8d6ae880d50f540a6
Keny <dedkeny@pop-os.lan>
2024-10-15 20:49:02 -0400

    formating corrections

--- hello_there/src/main.rs
+++ hello_there/src/main.rs
@@ -6,15 +6,27 @@ fn main() {
     /*
       1.1  - Comments, IYKYK
     */
-    println!("Hello, world!");
-    println!("I'm a Rustagen");
+    println!("\nHello, world!");
+    println!("I'm a Rustagen\n\n");
     // 1.2  - Formatted Print
     let x = 5 + /* 90 + */ 5;
-    println!("Comment Question:\n\tlet x = 5 + /* 90 + */ 5;\n");
-    println!("Is 'x' 10 or 100?\n\tx = {}\n", x);
+    println!("Comment Question:\n    let x = 5 + /* 90 + */ 5;");
+    println!("Is 'x' 10 or 100?\n    x = {}\n\n", x);
 
-    println!("{} was an int before being stringified into thi statement", 31);
-    println!("{0} is before {1} because {1}st place doesn't matter", 0, 1);
+    println!("{} was an int before being stringified into this statement\n\n", 31);
+
+    println!("{0} is before {1} because {1}st place doesn't matter\n\n", 0, 1);
+
+    println!("The {subject} is {verb} by its' {object}\n\n",
+        object = "1. VARIABLES",
+        subject = "2. SENTENCE",
+        verb = "3. JUDGED");
+
+    println!("Base 10  {{}} (Decimal)     : {}",   65535); // 65535
+    println!("Base 2  {{:b}}(binary)      : {:b}", 65535); // 1111111111111111
+    println!("Base 8  {{:o}}(octal)       : {:o}", 65235); // 177323
+    println!("Base 16 {{:x}}(hexadecimal) : {:x}", 65535); // ffff
+    println!("\n");
 
 
 }
